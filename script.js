8dfa7f0ffc7832d77be206f1515e6190b3b8082d
StrawbethyMilkshake <Bethany@strawbethy.com>
2023-07-09 13:38:56 +0100

    Fixes from creating NeoMarthy Site

--- script.js
+++ script.js
@@ -161,17 +161,21 @@ function generateGradientStyle(gradient) {
 
 function pageRightsize() {
   const aspectRatio = window.innerWidth / window.innerHeight;
+  const logoContainer = document.getElementById('logo-container');
   const linksContainer = document.getElementById("links-container");
   const footer = document.querySelector("footer");
 
   if (aspectRatio < 1) {
     linksContainer.style.marginLeft = "10%";
     linksContainer.style.marginRight = "10%";
+    logoContainer.classList.add("large");
     footer.classList.add("unpinned"); // Add the "unpinned" class
+    
   } else {
     linksContainer.style.marginLeft = "30%";
     linksContainer.style.marginRight = "30%";
     footer.classList.remove("unpinned"); // Remove the "unpinned" class
+    logoContainer.classList.remove("large");
   }
 }
 
